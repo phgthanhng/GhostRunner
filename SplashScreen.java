a5d528c71f77af149a74f9321abf172d85d06abc
reimarrosas <reimarrosas0208@gmail.com>
2020-11-13 12:16:08 -0500

    minimum classes for the game project

---
+++ SplashScreen.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SplashScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SplashScreen extends World
+{
+
+    /**
+     * Constructor for objects of class SplashScreen.
+     * 
+     */
+    public SplashScreen()
+    {    
+        // Create a new world with 10x10 cells with a cell size of 50x50 pixels.
+        super(13, 13, 50); 
+    }
+}
