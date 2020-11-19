5b546fabd3a8992f0b43891b2168dd9acb09c27c
Reimar Rosas <reimarrosas0208@gmail.com>
2020-11-19 16:28:25 -0500

    Added a selector framework that can be used in any world to choose object and transition to other worlds

--- Selector.java
+++ Selector.java
@@ -8,12 +8,46 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Selector extends Actor
 {
+    private int selectX = 0;
+    private int selectY = 0;
+    
+    public void selectX(int selectX)
+    {
+        this.selectX = selectX;
+    }
+    
+    public void selectY(int selectY)
+    {
+        this.selectY = selectY;
+    }
+    
+    public int getSelectX()
+    {
+        return selectX;
+    }
+    
+    public int getSelectY()
+    {
+        return selectY;
+    }
+    
     /**
      * Act - do whatever the Selector wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        if (Greenfoot.isKeyDown("right"))
+        {
+            selectX = 0;
+            selectY = 0;
+        }
+        else if (Greenfoot.isKeyDown("left"))
+        {
+            selectX = 0;
+            selectY = 0;
+        }
+        
+        setLocation(selectX, selectY);
     }    
 }
