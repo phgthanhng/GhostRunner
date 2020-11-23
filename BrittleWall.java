ab44869d43cb38edb0adb975bd1ee8c54b90d4fd
Reimar Rosas <reimarrosas0208@gmail.com>
2020-11-23 09:26:29 -0500

    readded the BrittleWall and UnbreakableWall classes as a subclass of Wall class

--- BrittleWall.java
+++ BrittleWall.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class BrittleWall extends Actor
+public class BrittleWall extends Wall
 {
     /**
      * Act - do whatever the BrittleWall wants to do. This method is called whenever
