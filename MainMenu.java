8c08493e486f8a66e3d23b14cffffd59fee90e54
danichh <70587707+danichh@users.noreply.github.com>
2020-11-24 13:57:02 -0500

    ADD the font

--- MainMenu.java
+++ MainMenu.java
@@ -9,23 +9,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MainMenu extends World
 {
 
-    /**
+/**
      * Constructor for objects of class MainMenu.
      * 
      */
     public MainMenu()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(650, 650, 1); 
+        super(650, 650, 1);
+        showTextWithPixeledFont("GHOST RUNNER", 50, 50, 150); 
     }
     
     /**
  * 
  */
-public void showTextWithBigRedFont(String message, int x, int y)
+public void showTextWithPixeledFont(String message, int size, int x, int y)
 {
-    getBackground().setFont( new  Font(50));
-    getBackground().setColor(Color.RED);
+    getBackground().setFont( new  Font("Pixeled",size));
+    getBackground().setColor(Color.WHITE);
     getBackground().drawString(message, x, y);
 }
 
